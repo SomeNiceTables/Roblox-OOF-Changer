f7ff9a6e28d2352f0a141bb6561da17b4deb4413
SomeNiceTables <104398685+SomeNiceTables@users.noreply.github.com>
2022-09-14 19:46:59 -0400

    fixing some stuff
    fixed roblox version and changed something

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import urllib.request, json, requests, getpass, shutil
 print("Checking Roblox version")
 
-with urllib.request.urlopen("https://api.whatexploitsare.online/logs/Comet/1") as url:
+with urllib.request.urlopen("https://api.whatexploitsare.online/logs/Synapse/1") as url:
     data = json.loads(url.read().decode())
 
 for i in range(len(data)):
@@ -13,7 +13,7 @@ for i in range(len(data)):
 
 cometstuff = dictionary['1']
 
-print("Downloading normal oof file")
+print("Downloading normal OOF file")
 
 request = requests.get("https://cdn.discordapp.com/attachments/985478681793282128/1001625277740351488/uuhhh-1.ogg")
 open("ouch.ogg", "wb").write(request.content)
@@ -22,7 +22,7 @@ user = getpass.getuser()
 destination1 = r"C:\Users\\"
 destination2 = "\AppData\Local\Roblox\Versions\\"
 
-print("Replace new OOF File with old one")
+print("Replacing new OOF File with old one")
 
 shutil.move("ouch.ogg", destination1 + user + destination2 + cometstuff["roblox_version"] + "\content\sounds\ouch.ogg")
 input("Done (press enter to exit)")
