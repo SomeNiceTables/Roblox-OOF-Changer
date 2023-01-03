afef891179ea0a705b96e080d3c6adad8ba965b7
SomeNiceTables <104398685+SomeNiceTables@users.noreply.github.com>
2023-01-03 01:42:16 -0500

    Made it so that this actually works

--- main.py
+++ main.py
@@ -6,17 +6,17 @@
 import urllib.request, json, requests, getpass, shutil
 print("Checking Roblox version")
 
-with urllib.request.urlopen("https://sirhurt.net/status/fetch.php?exploit=SirHurt%20V4") as url:
+with urllib.request.urlopen("https://api.whatexploitsare.online/logs/Synapse/1") as url:
     data = json.loads(url.read().decode())
 
 for i in range(len(data)):
     dictionary = data[i]
 
-cometstuff = dictionary["ROBLOX"]
+cometstuff = dictionary["1"]
 
 print("Downloading normal OOF file")
 
-request = requests.get("https://raw.githubusercontent.com/SomeNiceTables/Roblox-OOF-Changer/main/oof.ogg)
+request = requests.get("https://raw.githubusercontent.com/SomeNiceTables/Roblox-OOF-Changer/main/oof.ogg")
 open("ouch.ogg", "wb").write(request.content)
 user = getpass.getuser()
 
@@ -25,5 +25,5 @@ destination2 = "\AppData\Local\Roblox\Versions\\"
 
 print("Replacing new OOF File with old one")
 
-shutil.move("ouch.ogg", destination1 + user + destination2 + cometstuff["version"] + "\content\sounds\ouch.ogg")
+shutil.move("ouch.ogg", destination1 + user + destination2 + cometstuff["roblox_version"] + "\content\sounds\ouch.ogg")
 input("Done (press enter to exit)")
