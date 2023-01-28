e04689dd5e63c4670c05b1485f45e78c9ea14a7d
SomeNiceTables <104398685+SomeNiceTables@users.noreply.github.com>
2023-01-28 17:39:54 -0500

    Fixed other bug, check readme for errors stuff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Made By SomeNiceTables#9850
 # you must have the latest version of roblox for it to work
 # also make sure the script is up to date if it doesnt work, if it doesnt work contact me
+# Some error messages are listed in the readme file and solutions to solve it
 
 import urllib.request, json, requests, getpass, shutil
 print("Checking Roblox version")
@@ -20,10 +21,10 @@ request = requests.get("https://raw.githubusercontent.com/SomeNiceTables/Roblox-
 open("ouch.ogg", "wb").write(request.content)
 user = getpass.getuser()
 
-destination1 = r"C:\Users\\"
-destination2 = "\AppData\Local\Roblox\Versions\\"
+# destination1 = r"C:\Users\\"
+# destination2 = "\AppData\Local\Roblox\Versions\\"
 
 print("Replacing new OOF File with old one")
 
-shutil.move("ouch.ogg", destination1 + user + destination2 + cometstuff["version"] + "\content\sounds\ouch.ogg")
+shutil.move("ouch.ogg", "C:\Program Files (x86)\Roblox\Versions\\" + cometstuff["version"] + "\content\sounds\ouch.ogg")
 input("Done (press enter to exit)")
